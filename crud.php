435a11ac16b1b90addf0076da6a8888c1e355a47
parwizdanyar <perwezdanyar@gmail.com>
2015-04-25 09:29:28 +0430

    homework
    online upload and download videos

---
+++ crud.php
@@ -0,0 +1,122 @@
+﻿<?php
+// author: parwiz danyar perwezdanyar@gmail.com
+// include db
+require_once("db.php");
+
+
+class crud{
+	
+	//insert function
+	function insert($table, $column, $value){
+	
+		$sql="Insert Into $table ($column) values($value)";
+		echo "<h6>".$sql."</h6>";
+		$rs=mysqli_query($GLOBALS['DB'], $sql)or die(mysqli_error());
+		return $rs;
+		
+	}
+
+
+	//delete function
+	function delete($table , $condition){
+	
+		if($condition == ""){
+			$sql ="delete  from $table";
+		}else{
+			$sql ="delete  from $table where $condition";
+		}
+		//$sql=substr($sql,0,strlen($sql)-5);
+		$rs=mysqli_query($GLOBALS['DB'],$sql) or die(mysqli_error());
+		
+		return $rs;
+	
+	}
+	
+	// update
+	function update($table, $value, $condition){
+	
+		if($condition == ""){
+			$sql = "update $table set $value";
+		}else{
+			$sql = "update $table set $value where $condition";
+		}
+	echo $sql."<br>"."<h1>sdfsdfsfsdsdfsfd</h1>";
+		
+		$rs = mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error());
+		
+		return $rs;
+		
+	}
+
+
+	// select query with three parameter table , column and condition
+
+	function select($table,$column,$condition){
+             
+		if($condition == ""){
+			$sql  = "select $column from $table";
+        }else{
+			$sql  = "select $column from $table where $condition";
+		}
+		
+		//echo "<h5>".$sql."<br></h5>";
+		
+		$rs=mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error());
+		$rows;
+		
+		while($row = mysqli_fetch_assoc($rs)){
+			$rows[] = $row;
+		}	
+		
+		return $rows;
+		
+  
+	}
+	
+	//select query with limit
+	function select_limit($table,$column,$limit){
+	
+		$sql  = "select $column from $table $limit";
+		echo $sql;
+		$rs=mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error());
+		$rows;
+		
+		while($row = mysqli_fetch_assoc($rs)){
+		if(mysqli_num_rows($rs)>0){
+			$rows[] = $row;
+		}		
+		}	
+		
+		return $rows;
+		
+  
+	}
+	
+	//select latest record
+	function select_latest($table, $column){
+
+		$sql  = "select $column from $table order by id desc limit 1";
+		echo "<br/><h1>".$sql."</h1>";
+		$rs=mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error());
+		$row = mysqli_fetch_assoc($rs);
+		return $row;
+  
+	}
+	
+		//select latest record
+	function select_latest_manual($table, $column,$limit){
+
+		$sql  = "select $column from $table $limit";
+		echo $sql;
+		$rs=mysqli_query($GLOBALS['DB'], $sql) or die(mysqli_error());
+		$row = mysqli_fetch_assoc($rs);
+		return $row;
+  
+	}
+	
+	
+	
+  
+}
+
+?>
---
+++ db.php
@@ -0,0 +1,64 @@
+﻿<?php
+// author: parwiz danyar perwezdanyar@gmail.com
+// set true if production environment else false for development
+define('IS_ENV_PRODUCTION', true);
+
+// configure error reporting options
+if (!IS_ENV_PRODUCTION){
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+}
+
+ini_set('display_errors', !IS_ENV_PRODUCTION);
+ini_set('error_log', '../log/error.txt');
+
+
+// set time zone to use date/time functions without warnings
+date_default_timezone_set('Asia/Kabul');
+
+// This file contains the database access information.
+// This file also establishes a connection to MySQL and selects the database.
+// This file also defines the escape_data() function.
+
+// Set the database access information as constants.
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_SCHEMA', 'videos_audios');
+
+
+// Make the connection.
+if (!$GLOBALS['DB'] = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)) {
+    die('Could not connect to MySQLi: ' . mysqli_error());
+}
+
+// Select the database.
+if (!@mysqli_select_db($GLOBALS['DB'], DB_SCHEMA)) {
+    @mysqli_close($GLOBALS['DB']);
+    die('Could not select the database: ' . mysqli_error());
+} else {
+    @mysqli_query($GLOBALS['DB'], "SET NAMES utf8");
+    @mysqli_query($GLOBALS['DB'], "SET CHARACTER SET utf8");
+	mysqli_set_charset($GLOBALS['DB'], 'utf8');
+}
+
+// Create a function for escaping the data.
+function escape_data($data)
+{
+
+    // Address Magic Quotes.
+    if (ini_get('magic_quotes_gpc')) {
+        $data = stripslashes($data);
+    }
+
+    // Check for mysql_real_escape_string() support.
+    if (function_exists('mysqli_real_escape_string')) {
+        $data = mysqli_real_escape_string($GLOBALS['DB'], trim($data));
+    } else {
+        $data = mysqli_escape_string($GLOBALS['DB'], trim($data));
+    }
+    // Return the escaped value.
+    return $data;
+
+} // End of escape_data()
+
+?>
